f193756c3efa6869f318da0634066889b7a25d79
baobaohuang <huangnf95@gmail.com>
2015-05-13 16:13:21 +0800

    codepage
    codepage

---
+++ codepageBig5UTF8/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace codepageBig5UTF8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            //string source = "⊃;nÅé&frac14;Ò⊃;Õ¤¤ªº¤@¯ë©Ê¿ù»~¡G&frac14;Ð·Ç GUI (⊃;Ï§Î¤Æ¥Î¤á¤¶­±)¡C";
+
+            string input = System.IO.File.ReadAllText(@"C:\aa.txt",Encoding.GetEncoding(950));
+
+            //checkCodePage(input);
+            big5toutf8(input);
+            //CodePageList();
+
+        }
+
+        static void checkCodePage(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var e1 in Encoding.GetEncodings())
+            {
+                foreach (var e2 in Encoding.GetEncodings())
+                {
+                    byte[] unknow = Encoding.GetEncoding(e1.CodePage).GetBytes(input);
+                    string result = Encoding.GetEncoding(e2.CodePage).GetString(unknow);
+                    sb.AppendLine(string.Format("{0} => {1} : {2}", e1.CodePage, e2.CodePage, result));
+                }
+            }
+            File.WriteAllText(@"C:\Users\frankhuang\Downloads\bb.txt", sb.ToString());
+        }
+        static void big5toutf8(string input)
+        {
+            //1200 utf-16  65001 utf-8
+            StringBuilder sb = new StringBuilder();
+            byte[] unknow = Encoding.GetEncoding(1200).GetBytes(input);
+            byte[] unknow2 = Encoding.GetEncoding(950).GetBytes(input);
+            string result = Encoding.GetEncoding(65001).GetString(unknow);
+            sb.AppendLine(result);
+
+            File.WriteAllText(@"C:\bb.txt", sb.ToString());
+        }
+        static void CodePageList()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (EncodingInfo ei in Encoding.GetEncodings())
+            {
+                Encoding e = ei.GetEncoding();
+                sb.AppendLine(string.Format ("Name:{0},CodePage:{1}", ei.Name, e.CodePage));
+               // Console.WriteLine("Name:{0},CodePage:{1}", ei.Name, e.CodePage);               
+            }
+            File.WriteAllText(@"C:\cc.txt", sb.ToString());
+        
+        }
+
+    }
+}
